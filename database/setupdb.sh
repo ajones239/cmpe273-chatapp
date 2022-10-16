f2b9803e8004bd422ffb5df29b26161a1859c0a9
Aidan Jones <arjones239@protonmail.ch>
2022-10-16 03:01:20 -0700

    added some log messages

--- database/setupdb.sh
+++ database/setupdb.sh
@@ -3,24 +3,32 @@
 if [ -d $(which python3) ]; then
     echo Cannot find python3
     exit 1
+else
+    echo "Found python3"
 fi
 
 if ! [ -d venv ]; then
+    echo "venv not found, creating virtual env..."
     python3 -m venv venv
     source ./venv/bin/activate
+    echo "installing requirements..."
     python3 -m pip install -r requirements.txt
 else
+    echo "venv found, activating..."
     source ./venv/bin/activate
 fi
 
 if [[ $(netstat -tl | grep 27017) ]]; then
+    echo "mongodb is running, stopping cluster..."
     mlaunch stop
 fi
 
 if [ -d ./data ]; then
+    echo "clearing old cluster data..."
     rm -rf ./data
 fi
 
+echo "launching mongodb cluster..."
 mlaunch \
     --replicaset \
     --nodes 3 \
@@ -30,9 +38,11 @@ mlaunch \
     --config 3 \
     --dir ./data 
 
+echo "mongodb cluster successfully launched"
 mlaunch list
 sleep 2
 
+echo "adding user..."
 echo "
 use admin
 db.createUser(
